afb9b554d1b5f775cc5359a52638dfcbd25f46ce
Felipe Sales <sales.felipeb@gmail.com>
2017-02-28 14:16:42 -0300

    Removendo conflito em Simple Statement

--- test/Test.pas
+++ test/Test.pas
@@ -1,5 +1,13 @@
-program HelloWorld;
-begin
-    writeln('Hello World!');
-    readkey;
-end.
+Program Lesson1_Program3;
+Var
+    Num1, Num2, Sum : Integer;
+
+Begin {no semicolon}
+	Write('Input number 1:');
+	Readln(Num1);
+	Writeln('Input number 2:');
+	Readln(Num2);
+	Sum := Num1 + Num2; {addition}
+	Writeln(Sum);
+	Readln;
+End. 
