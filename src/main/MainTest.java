d3a9b194d685db82df8674ce07736fb686207124
Felipe Sales <sales.felipeb@gmail.com>
2017-02-28 22:19:02 -0300

    Ajeitando ambiguidade em Types

--- src/main/MainTest.java
+++ src/main/MainTest.java
@@ -10,12 +10,14 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class MainTest {
+	public static final String ANSI_RESET = "\u001B[0m";
+  public static final String ANSI_GREEN = "\u001B[32m";
 
 	public static void main(String[] args) throws IOException {
 		String filePath = "test/Test.pas";
 		Scanner scanner = null;
 		try {
-			scanner = new Scanner(new BufferedReader(new FileReader(filePath)));
+			scanner = new Scanner(new BufferedReader(new FileReader(args[0])));
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
@@ -23,7 +25,7 @@ public class MainTest {
 		Symbol s = null;
 		try {
 			s = parser.parse();
-			System.out.println("The compilation process was successfully finished!");
+			System.out.println(ANSI_GREEN + "The compilation process was successfully finished!" + ANSI_RESET);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
