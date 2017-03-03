b144e7c7d34d0096dd8e8372ddfb8fd831b2927c
Felipe Sales <sales.felipeb@gmail.com>
2017-03-02 23:15:41 -0300

    Adicionando novos casos de teste

---
+++ test/TutorialsPoint/arithmetic_op.pas
@@ -0,0 +1,27 @@
+program calculator;
+var
+a,b,c : integer;
+d: real;
+
+begin
+   a:=21;
+   b:=10;
+   c := a + b;
+
+   writeln(' Line 1 - Value of c is ', c );
+   c := a - b;
+
+   writeln('Line 2 - Value of c is ', c );
+   c := a * b;
+
+   writeln('Line 3 - Value of c is ', c );
+   d := a / b;
+
+   writeln('Line 4 - Value of d is ', d:3:2 );
+   c := a mod b;
+
+   writeln('Line 5 - Value of c is ' , c );
+   c := a div b;
+
+      writeln('Line 6 - Value of c is ', c );
+end.
---
+++ test/TutorialsPoint/op-precedence.pas
@@ -0,0 +1,22 @@
+program opPrecedence;
+var
+a, b, c, d : integer;
+e: real;
+
+begin
+   a := 20;
+   b := 10;
+   c := 15;
+   d := 5;
+   e := (a + b) * c / d;      (* ( 30 * 15 ) / 5 *)
+   writeln('Value of (a + b) * c / d is : ',  e:3:1 );
+
+   e := ((a + b) * c) / d;    (* (30 * 15 ) / 5  *)
+   writeln('Value of ((a + b) * c) / d is  : ' ,  e:3:1 );
+
+   e := (a + b) * (c / d);   (*  (30) * (15/5)  *)
+   writeln('Value of (a + b) * (c / d) is  : ',  e:3:1);
+
+   e := a + (b * c) / d;     (*  20 + (150/5)  *)
+   writeln('Value of a + (b * c) / d is  : ' ,  e:3:1 );
+end.
