3b780f6feecade05d91ad679ad6d810f963c3a32
rafaelrds <crafards@gmail.com>
2017-03-04 01:16:32 -0300

    fix enmered-types.pas #pas

--- test/TutorialsPoint/enumered-types.pas
+++ test/TutorialsPoint/enumered-types.pas
@@ -2,8 +2,12 @@ program exEnumeration;
 type
 beverage = (coffee, tea, milk, water, coke, limejuice);
 
+
+var
+r, d, c : real;
 var
 drink:beverage;
+
 
 begin
    writeln('Which drink do you want?');
