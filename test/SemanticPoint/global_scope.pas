416b4c2ff171f0c5d75b9b26e607d72bfdb2cb8d
analuizamtg <analuizamtg@gmail.com>
2017-03-15 16:55:48 -0300

    adding more tests

---
+++ test/SemanticPoint/global_scope.pas
@@ -0,0 +1,18 @@
+program exFunction;
+var
+   a, b, ret : integer;
+
+function max(num1, num2: integer): integer;
+begin
+   ret := 50;
+   c := 100;
+   (* not previously declared *)
+   d := 200;
+   e := 20;
+end;
+
+begin
+   a := 100;
+   b := 200;
+   max(a, b);
+end.
