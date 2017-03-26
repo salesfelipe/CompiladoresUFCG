bf58bb64ffbeba5adccaa3b80679bc680976fd8a
Felipe Sales <sales.felipeb@gmail.com>
2017-03-26 10:57:29 -0300

    Checagem semantica do repeat

---
+++ src/compiler/exceptions/InvalidRepeatStatement.java
@@ -0,0 +1,10 @@
+package compiler.exceptions;
+
+/**
+ * Created by felipesales on 26/03/17.
+ */
+public class InvalidRepeatStatement  extends Exception {
+    public InvalidRepeatStatement(String message) {
+        super(message);
+    }
+}
