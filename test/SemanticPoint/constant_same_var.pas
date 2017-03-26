4aa7995e5d4ad065ee2380f4be0f7bc1b841b48b
rafaelrds <crafards@gmail.com>
2017-03-26 17:19:01 -0300

    finishing syntax analysis tests

---
+++ test/SemanticPoint/constant_same_var.pas
@@ -0,0 +1,11 @@
+program const_circle (input,output);
+const
+PI = 3.141592654;
+
+var
+PI, r, d, c : real;   {variable declaration: radius, dia, circumference}
+
+begin
+   d := 2 * r ;
+   c :=  PI * d;
+end.
---
+++ test/SemanticPoint/non_existent_function_call.pas
@@ -0,0 +1,27 @@
+program exFunction;
+var
+   a, b, max : integer;
+
+(*function definition *)
+function ret(num1, num2: integer; s1, s2, s3: string): integer;
+var
+   (* local variable declaration *)
+   result: integer;
+   c : integer;
+
+begin
+   if (num1 > num2) then
+      result := num1
+
+   else
+      result := num2;
+   ret := result;
+end;
+
+begin
+   a := 100;
+   b := 200;
+   ret := 100;
+   ret := max(a, b);
+
+end.
