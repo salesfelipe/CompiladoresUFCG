88ca8e1eb1eeb0ffeb07982bb150b6a8591738f1
rafaelrds <crafards@gmail.com>
2017-03-29 13:56:54 -0300

    Dear Billy, I just added a test.pas for the sabatina

--- test/test.pas
+++ test/test.pas
@@ -1,7 +1,29 @@
 program test;
+const
+   PI = 3.141592654;
 var
+   a, d : integer;
    c: real;
+   s :string;
+   b : boolean;
+function max(num1, num2: integer): integer;
+var
+   (* local variable declaration *)
+   result: integer;
 
 begin
-   c := 3 + -2 ;
+   if (num1 > num2) then
+      result := num1
+
+   else
+      result := num2;
+   max := result;
+end;
+{inicio do programa}
+begin
+   a := 42;
+   c := 3 + - 2 ;
+   s := 'AnaFelipeRafael';
+   b := true and not (5 > 6);
+   c := max(-1, a);
 end.
